89741d4cc0631f51f9c2690f222b0b9458a0f28a
Stefan Andrei <andreistefanciprian@gmail.com>
2025-09-29 17:49:32 +0930

    gRPC implementation url-read

---
+++ url-read/internal/implementation/urlread.go
@@ -0,0 +1,53 @@
+package urlread
+
+import (
+	"context"
+
+	repo "github.com/andreistefanciprian/urlshortener/url-read/internal/db"
+	uread "github.com/andreistefanciprian/urlshortener/url-read/proto"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Implementation struct {
+	logger *logrus.Logger
+	repo   repo.URLRepository
+	uread.UnimplementedURLReaderServer
+}
+
+func NewUrlReadImplementation(logger *logrus.Logger, repository repo.URLRepository) *Implementation {
+	return &Implementation{
+		logger: logger,
+		repo:   repository,
+	}
+}
+
+func (s *Implementation) GetLongURL(ctx context.Context, request *uread.LongURLRequest) (*uread.LongURLResponse, error) {
+	s.logger.WithContext(ctx).WithFields(logrus.Fields{
+		"shortCode": request.ShortUrl,
+	}).Info("Processing long URL read request")
+
+	// Check if context is cancelled
+	if ctx.Err() != nil {
+		s.logger.WithContext(ctx).Error("Request context cancelled")
+		return nil, ctx.Err()
+	}
+
+	// Retrieve the original URL from the database (context will be used by db layer)
+	response, err := s.repo.GetLongURL(ctx, request.ShortUrl)
+	if err != nil {
+		s.logger.WithContext(ctx).WithError(err).WithFields(logrus.Fields{
+			"shortCode": request.ShortUrl,
+		}).Error("Failed to retrieve original URL")
+		return nil, err
+	}
+
+	s.logger.WithContext(ctx).WithFields(logrus.Fields{
+		"shortCode":   request.ShortUrl,
+		"originalURL": response.OriginalURL,
+	}).Info("Successfully retrieved original URL")
+	return &uread.LongURLResponse{
+		LongUrl:    response.OriginalURL,
+		Expiration: timestamppb.New(*response.ExpiresAt),
+	}, nil
+}
