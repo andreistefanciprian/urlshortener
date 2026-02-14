85f2220b5438c64903302e6495a2a2b3c5e00700
Stefan Andrei <andreistefanciprian@gmail.com>
2026-02-14 15:01:38 +1030

    fix missing bracket

--- terraform/networking/provider.tf
+++ terraform/networking/provider.tf
@@ -4,9 +4,10 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = "~> 7.0"
+    }
   }
-
 }
+
 provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
