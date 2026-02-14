c213740d3346eef165b40ead1e49d85c25490ba6
Stefan Andrei <andreistefanciprian@gmail.com>
2026-02-14 14:58:34 +1030

    implement wait_for_apis time sleep

--- terraform/gke/provider.tf
+++ terraform/gke/provider.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 7.0" # OIDC https://github.com/hashicorp/terraform-provider-google/releases/tag/v3.61.0
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.13"
+    }
   }
 
 }
