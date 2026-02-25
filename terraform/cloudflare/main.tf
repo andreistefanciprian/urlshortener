2cb6c36ed3dae428aa60d04a585aa43174c59357
Stefan Andrei <andreistefanciprian@gmail.com>
2026-02-25 20:40:33 +1030

    upgrade cloudflare provider to v5

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -5,13 +5,13 @@ resource "random_id" "tunnel_secret" {
 
 # Cloudflare Tunnel
 resource "cloudflare_zero_trust_tunnel_cloudflared" "gke" {
-  account_id = var.cloudflare_account_id
-  name       = "${var.project_name}-gke-tunnel"
-  secret     = random_id.tunnel_secret.b64_std
+  account_id    = var.cloudflare_account_id
+  name          = "${var.project_name}-gke-tunnel"
+  tunnel_secret = random_id.tunnel_secret.b64_std
 }
 
 # Route private network CIDR through the tunnel
-resource "cloudflare_zero_trust_tunnel_route" "gke" {
+resource "cloudflare_zero_trust_tunnel_cloudflared_route" "gke" {
   account_id = var.cloudflare_account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.gke.id
   network    = var.tunnel_route_cidr # 10.100.0.0/18
@@ -19,17 +19,30 @@ resource "cloudflare_zero_trust_tunnel_route" "gke" {
 }
 
 # WARP client split tunnel — route GKE private network through Cloudflare
-resource "cloudflare_zero_trust_split_tunnel" "gke" {
+resource "cloudflare_zero_trust_device_default_profile" "gke" {
   account_id = var.cloudflare_account_id
-  mode       = "include"
-  tunnels {
-    address     = var.tunnel_route_cidr # 10.100.0.0/18
-    description = "GKE private network (nodes, pods, services, master)"
+
+  # Proxy both Traffic and DNS through WARP
+  service_mode_v2 = {
+    mode = "warp"
   }
+
+  include = [
+    {
+      address     = var.tunnel_route_cidr # 10.100.0.0/18
+      description = "GKE private network (nodes, pods, services, master)"
+    },
+  ]
+}
+
+# Fetch tunnel token via data source (tunnel_token attribute removed in v5)
+data "cloudflare_zero_trust_tunnel_cloudflared_token" "gke" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.gke.id
 }
 
 # Write tunnel token into the secret shell created by the secrets layer
 resource "google_secret_manager_secret_version" "tunnel_token" {
   secret      = data.terraform_remote_state.secrets.outputs.cloudflared_secret_name
-  secret_data = cloudflare_zero_trust_tunnel_cloudflared.gke.tunnel_token
+  secret_data = data.cloudflare_zero_trust_tunnel_cloudflared_token.gke.token
 }
